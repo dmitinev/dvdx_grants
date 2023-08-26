d3ebe8e9322cb560104aaef3d595cc46b2d18369
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2023-08-26 20:15:37 +0200

    added swiper functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../css/style.scss'
-import 'swiper/css';
+import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
 
+window.addEventListener("load", () => {
+  new Swiper('.featuredGrants__slider', {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      prevEl: '.featuredGrants__button-prev',
+      nextEl: '.featuredGrants__button-next',
+    },
+  });
+})
